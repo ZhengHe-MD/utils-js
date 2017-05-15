7a7245607e8a8f6e820d1d26641f5d44c49fc463
ZhengHe-MD <zhenghe@madadata.com>
2017-05-15 21:31:51 +0800

    test prettier

--- src/shallowCopy.js
+++ src/shallowCopy.js
@@ -1,47 +1,47 @@
 // @flow
 
-type Target = Array<mixed> | Object;
+type Input = Array<mixed> | Object
 
-module.exports = function(target: Target): Target {
-  var copy;
+module.exports = function(target: Input): Input {
+  var copy
 
   if (isArray(target)) {
-    var len = target.length;
-    copy = new Array(len);
+    var len = target.length
+    copy = new Array(len)
     for (var i = 0; i < len; i++) {
-      copy[i] = target[i];
+      copy[i] = target[i]
     }
   } else {
-    var keys = objectKeys(target);
-    var len = keys.length;
-    copy = {};
+    var keys = objectKeys(target)
+    var len = keys.length
+    copy = {}
 
     for (var i = 0; i < len; i++) {
-      var key = keys[i];
+      var key = keys[i]
       // $FlowFixMe
-      copy[key] = target[key];
+      copy[key] = target[key]
     }
   }
-  return copy;
-};
+  return copy
+}
 
-var toString = Object.prototype.toString;
-var hasOwnProperty = Object.prototype.hasOwnProperty;
+var toString = Object.prototype.toString
+var hasOwnProperty = Object.prototype.hasOwnProperty
 
 var isArray =
   Array.isArray ||
   function(xs) {
-    return toString.call(xs) === "[object Array]";
-  };
+    return toString.call(xs) === "[object Array]"
+  }
 
 var objectKeys =
   Object.keys ||
   function(obj) {
-    var keys = [];
+    var keys = []
     for (var key in obj) {
       if (hasOwnProperty.call(obj, key)) {
-        keys.push(key);
+        keys.push(key)
       }
     }
-    return keys;
-  };
+    return keys
+  }
